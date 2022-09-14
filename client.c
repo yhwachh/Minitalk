e94686cd4a95f2489a7f1d365454e07523931809
Ibraguim Albakov <ibraguimalbakov@MacBook-Air-de-Ibraguim.local>
2022-09-14 15:48:57 +0200

    2st commit

--- client.c
+++ client.c
@@ -2,94 +2,6 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include "libft/libft.h"
-/*
-static void	action(int sig)
-{
-	static int	received = 0;
-
-	if (sig == SIGUSR1)
-		++received;
-	else
-	{
-		ft_putnbr_fd(received, 1);
-		ft_putchar_fd('\n', 1); 
-		exit(0);
-	}
-}
-static void	mt_kill(int pid, char *str)
-{
-	char	c;
-	int		j = 128;       // 128 = 10000000 en bit, si / 2 -> 64 = 01000000 etc..
-	char	new = 0;
-	int		f = 0;
-	int		i ;
-
-	while (str[f])
-	{
-		c = str[f];
-		while (j > 0)
-		{								// >> mouvement de byte ver la droite 
-			if ((c & j) > 0)
-			{
-				new = new | j;				// & -> compare les byte et renvoi les byte ou il y a 1 dans les deux
-				kill(pid, SIGUSR2);
-			}								// comment envoyer les lettres -> grasse a kill par byte
-			else
-				kill(pid, SIGUSR1);
-			usleep(100);
-			j /= 2;				// pour reduire le charge de travaille, pour que le programme puise faire certain cycle
-		}
-		new = 0;
-		j = 128;
-		f++;
-	}
-	i = 8;
-	while (i--)						// combien de caractere reçu;
-	{
-		kill(pid, SIGUSR1);
-		usleep(100);
-	}
-}
-
-int	main(int argc, char **argv)
-{
-	if (argc != 3 || !ft_strlen(argv[2]))
-		return (1);
-	ft_putstr_fd("Sent    : ", 1);
-	ft_putnbr_fd(ft_strlen(argv[2]), 1);
-	ft_putchar_fd('\n', 1);
-	ft_putstr_fd("Received: ", 1);
-	signal(SIGUSR1, action);		// peut pas bloque les signales
-	signal(SIGUSR2, action);
-	mt_kill(ft_atoi(argv[1]), argv[2]);
-	while (1)
-		pause();
-	return (0);
-}*/
-/*
-void	send_bit_by_bit(int pid, char *argv)
-{
-	size_t	len;
-	size_t	i;
-	int		shift;
-
-	len = ft_strlen(argv);
-	i = 0;
-	while (i < len)
-	{
-		shift = 0;
-		while (shift < 8)
-		{
-			if ((argv[i] >> shift) & 1)
-				kill(pid, SIGUSR2);
-			else
-				kill(pid, SIGUSR1);
-			shift++;
-			usleep(100);
-		}
-		i++;
-	}
-}*/
 
 void	send_bit_by_bit(int pid, char *argv)
 {
@@ -121,13 +33,9 @@ int	main(int argc, char **argv)
 	if (argc == 3)
 	{
 		send_bit_by_bit(ft_atoi(argv[1]), argv[2]);
+		send_bit_by_bit(ft_atoi(argv[1]), argv[write(1 , "\n", 2)]);
 	}
 	else
 		write(1, "./client 'pid' 'message'\n", 25);
 	return (0);
 }
-
-
-// https://www.youtube.com/watch?v=PErrlOx3LYE&list=RDCMUC6qj_bPq6tQ6hLwOBpBQ42Q&index=2
-
-// https://www.youtube.com/watch?v=jF-1eFhyz1U&list=RDCMUC6qj_bPq6tQ6hLwOBpBQ42Q&index=3
--- server.c
+++ server.c
@@ -1,52 +1,6 @@
 #include <signal.h>
 #include <unistd.h>
 #include "libft/libft.h"
-/*
-static void	action(int sig, siginfo_t *info, void *context) // 1) numereau de signal 2) la structure 3) osef
-{
-	static int				i = 0;
-	static pid_t			client_pid = 0;
-	static unsigned char	c = 0;
-
-	(void)context;   // osef
-	if (!client_pid)
-		client_pid = info->si_pid;		// 																	typedef struct {
-	c |= (sig == SIGUSR2);				//	(sur deux byte mes 1 si il ya au moin un 1 dans l'un des deux )	pid_t si_pid ; (pross id) il contient le numereau du signal reçu , il remplit auto
-	if (++i == 8)						//																	} siginfo_t
-	{
-		i = 0;
-		if (!c)
-		{
-			kill(client_pid, SIGUSR2);
-			client_pid = 0;
-			return ;
-		}
-		ft_putchar_fd(c, 1);
-		c = 0;
-		kill(client_pid, SIGUSR1);
-	}
-	else
-		c <<= 1;
-}
-
-void	handler(int signum, struct __siginfo *info, void *vo);
-
-int	main(void)
-{
-	struct sigaction	s_sigaction;
-
-	ft_putstr_fd("Server PID: ", 1);
-	ft_putnbr_fd(getpid(), 1);
-	ft_putchar_fd('\n', 1);
-	s_sigaction.sa_sigaction = handler;		// ordre quand um signal passe par cette strusture , il dois le renvoyer ver la foction action
-	s_sigaction.sa_flags = SA_SIGINFO;		// elle permet a la foction d'avoir 3 parametre si non compile pas
-	sigaction(SIGUSR1, &s_sigaction, 0);	// peut bloque les signal , sigation stucture se trouve dans le header 
-	sigaction(SIGUSR2, &s_sigaction, 0);	// pour defenir quel signal passe (signalName) par quel structure(structName)  
-	while (1)
-		pause();
-	return (0);
-}*/
-
 
 void	get_bit_by_bit(int sig)
 {
